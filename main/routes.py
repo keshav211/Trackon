326d0b5393360b41b0402d69e5e92f5147239aeb
saranggupta11 <sarang.gupta0811@gmail.com>
2022-06-20 14:00:35 +0530

    user profile page and update feature added

--- main/routes.py
+++ main/routes.py
@@ -1,8 +1,11 @@
+import secrets
+import os
 from flask import render_template, url_for, flash, redirect, request
 from main import app, db, bcrypt
-from main.forms import RegistrationForm, LoginForm
+from main.forms import RegistrationForm, LoginForm,UpdateAccountForm
 from main.models import User
 from flask_login import login_user, current_user, logout_user, login_required
+from PIL import Image
 
 
 @app.route("/")
@@ -58,8 +61,34 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _,f_ext=os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,'static/images',picture_fn)
+    output_size = (125,125)
+    image = Image.open(form_picture)
+    image.thumbnail(output_size)
+    image.save(picture_path)
 
-@app.route("/account")
+    return picture_fn
+   
+
+@app.route("/account",methods=['GET','POST'])
 @login_required
 def account():
-    return render_template("account.html", title="Account")
+    image_file = url_for('static', filename='images/' + current_user.image_file)
+    form=UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash('Your account has been updated!','success')
+        return redirect(url_for('account'))
+    elif request.method=='GET':
+        form.username.data=current_user.username
+        form.email.data=current_user.email    
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
