0e0ff694bbcd7954b4a8465653470f4635fe15ab
saranggupta11 <sarang.gupta0811@gmail.com>
2022-07-08 02:25:24 +0530

    code refactoring and styling

--- main/models.py
+++ main/models.py
@@ -1,25 +1,30 @@
+from zoneinfo import ZoneInfo
 from main import db, login_manager
 from flask_login import UserMixin
 from datetime import datetime
+from dateutil.tz import gettz
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class Inputaken(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     task_title = db.Column(db.String(300), nullable=False)
     task_value = db.Column(db.Integer, nullable=False)
-    task_date = db.Column(db.DateTime, default=datetime.utcnow)
+    task_date = db.Column(
+        db.DateTime, default=datetime.now(tz=gettz('Asia/Kolkata')))
     task_variable = db.Column(db.String(20), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    tracker_id = db.Column(db.Integer, db.ForeignKey('tracker.sno'), nullable=False)
+    tracker_id = db.Column(db.Integer, db.ForeignKey(
+        'tracker.sno'), nullable=False)
 
     def __repr__(self) -> str:
         return f"(value:'{self.task_value}', timestamp:'{self.task_date}')"
 
-    def __getitem__(self,key):
+    def __getitem__(self, key):
         return self.__dict__[key]
 
 
@@ -31,10 +36,11 @@ class User(db.Model, UserMixin):
                            default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     trackers = db.relationship('Tracker', backref='user', lazy=True)
-    logs=db.relationship('Inputaken', backref='user', lazy=True)
+    logs = db.relationship('Inputaken', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
+
 
 class Tracker(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
